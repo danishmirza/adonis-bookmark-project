ad7458012b988205bbdf0d39d9b3b8e3a8fa9db2
Danish Mirza <mirzadanish3@gmail.com>
2023-09-05 19:51:23 +0500

    docker compose file and mysql entry file updated

--- .docker/mysql/mysql-dev-init.sql
+++ .docker/mysql/mysql-dev-init.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE IF NOT EXISTS `adonis_books_bookmarks` ;
 
+DROP USER 'adonis'@'%';
 CREATE USER 'adonis'@'%' IDENTIFIED BY 'secret';
 GRANT ALL PRIVILEGES ON * . * TO 'adonis'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
